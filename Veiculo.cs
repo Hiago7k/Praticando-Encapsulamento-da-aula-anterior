2272aec7f89c29cef4cccb4e8acd71186fe0799a
Hiago 7k <hiago7k@gmail.com>
2026-03-09 15:25:37 -0300

    criando classe veiculo

---
+++ Veiculo.cs
@@ -0,0 +1,8 @@
+﻿namespace Praticando_Encapsulamento_da_aula_anterior;
+
+internal class Veiculo
+{
+    private decimal velocidadeAtual;
+    public string Placa { get; set; }
+    public decimal VelocidadeAtual { get;}
+}
