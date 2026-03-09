301ba65c83ff785414d1baaaf9a33d53cf904836
Hiago 7k <hiago7k@gmail.com>
2026-03-09 16:23:20 -0300

    criando uma classe isolada

---
+++ HistoricoMedico.cs
@@ -0,0 +1,12 @@
+﻿namespace Praticando_Encapsulamento_da_aula_anterior;
+
+internal class HistoricoMedico
+{
+    public string CodigoProntuario { get; }
+
+
+    public void ExibirCodigo()
+    {
+        Console.WriteLine($"Codigo do Pontuario" {CodigoProntuario});
+    }
+}
