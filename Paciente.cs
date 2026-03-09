00f2e6c9ae7acb64e05db13998b9846a5743d691
Hiago 7k <hiago7k@gmail.com>
2026-03-09 16:24:03 -0300

    criando ctor da classe paciente

--- Paciente.cs
+++ Paciente.cs
@@ -3,6 +3,12 @@ namespace Praticando_Encapsulamento_da_aula_anterior;
 
 public class Paciente
 {
+    public Paciente(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+
     public string Nome { get; }
     public int Idade { get; }
 }
