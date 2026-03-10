8ebb69716c1cb44222e078be53ad04c4f40c692e
Hiago 7k <hiago7k@gmail.com>
2026-03-10 18:45:49 -0300

    implementando logica e criando metodo

--- SegurancaConta.cs
+++ SegurancaConta.cs
@@ -3,4 +3,18 @@ namespace Praticando_Encapsulamento_da_aula_anterior;
 
 internal class SegurancaConta
 {
+    public ContaBancaria MinhaConta;
+
+    public void ValidarSaque(double valor)
+    {
+        if (valor > 1000)
+        {
+            Console.WriteLine("Saque negado pela politica de seguranca.");
+        }
+        else 
+        {
+            Console.WriteLine("Saque realizado com sucesso.");
+        }
+    }
 }
+
