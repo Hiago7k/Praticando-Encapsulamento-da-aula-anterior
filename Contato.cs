4d84bf1221555287115348e4f26d16596a8bc2bb
Hiago 7k <hiago7k@gmail.com>
2026-03-10 20:09:16 -0300

    criando as propriedades dessa classe

--- Contato.cs
+++ Contato.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace Praticando_Encapsulamento_da_aula_anterior;
 
-namespace Praticando_Encapsulamento_da_aula_anterior
+internal class Contato
 {
-    internal class Contato
-    {
-    }
+    public string Nome { get; }
+    public string Telefone { get; }
 }
