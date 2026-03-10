191eb9038a4340858142922078c560e01b2bbca8
Hiago 7k <hiago7k@gmail.com>
2026-03-10 20:18:39 -0300

    implementando logica

--- Agenda.cs
+++ Agenda.cs
@@ -2,4 +2,22 @@
 
 internal class Agenda
 {
+    private readonly List<Contato>  contatos;
+    public string Proprietario { get; }
+
+
+
+    public bool AdicionarContato(Contato contato)
+    {
+        if (contatos.Contains(contato))
+        {
+            Console.WriteLine($"Contato com esse nome ja esta na agenda.");
+        }
+        else 
+        {
+            contatos.Add(contato);
+        }
+    }
+
+  
 }
