fdbb001dc16bf3a8ad4a91c879e2c56b774ff3e5
Hiago 7k <hiago7k@gmail.com>
2026-03-11 18:06:30 -0300

    criando construtor da classe

---
+++ Reserva.cs
@@ -0,0 +1,18 @@
+﻿namespace Praticando_Encapsulamento_da_aula_anterior;
+
+internal class Reserva
+{
+    private int diarias;
+
+    public Reserva(Hospede hospede, Quarto quarto, Quarto valorDiaria)
+    {
+        this.hospede = hospede;
+        this.quarto = quarto;
+        ValorDiaria = valorDiaria;
+    }
+
+    public Hospede hospede { get;}
+    public Quarto quarto { get;}
+    public Quarto ValorDiaria { get;}
+
+}
