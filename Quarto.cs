44c09e5e9c352b483833e3569631b957e1d45d9c
Hiago 7k <hiago7k@gmail.com>
2026-03-11 18:03:36 -0300

    criando classe quarto

---
+++ Quarto.cs
@@ -0,0 +1,8 @@
+﻿namespace Praticando_Encapsulamento_da_aula_anterior;
+
+internal class Quarto
+{
+    public int Numero { get; set; }
+    public double ValorDiaria { get; set; }
+    // somente valores Positivos
+}
