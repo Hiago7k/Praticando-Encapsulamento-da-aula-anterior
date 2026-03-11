9ea6f187fb807a76bf9d388256c78e9327835f5a
Hiago 7k <hiago7k@gmail.com>
2026-03-10 21:08:35 -0300

    criando propriedades e logica

--- Estudante.cs
+++ Estudante.cs
@@ -2,4 +2,14 @@
 
 internal class Estudante
 {
+    public string Nome { get;  }
+    public int Nota1 { get; set; }
+    public int Nota2 { get; set; }
+
+    public decimal Media { get;  }
+    // media de nota1 + nota2 / 2
+
+    public string Situacao { get;  }
+    // Aprovado nota maior ou igual a 6
+    // Reprovado se nota menor que 6
 }
