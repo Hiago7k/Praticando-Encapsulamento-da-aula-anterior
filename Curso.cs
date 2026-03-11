351932feff84fe51c69c89874a16051129dd8cae
Hiago 7k <hiago7k@gmail.com>
2026-03-10 21:53:01 -0300

    gerando ctor

--- Curso.cs
+++ Curso.cs
@@ -4,6 +4,11 @@ internal class Curso
 {
     private  List<Estudante> matricula = new List<Estudante>();
 
+    public Curso(string nome, int vagasTotais)
+    {
+        Nome = nome;
+        VagasTotais = vagasTotais;
+    }
 
     public string Nome { get;  }
     public int VagasTotais { get; }
@@ -11,7 +16,7 @@ internal class Curso
 
     public bool Matricular(Estudante estudante)
     {
-        if (matricula.Contains(VagasTotais))
+        if (matricula.Contains(vagasTotais))
         {
             // tem vaga
             return true;
